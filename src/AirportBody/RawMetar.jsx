b966537896e3142fc95395a222562d44f17fff6e
Jared <jbeckersalax9@aol.com>
2023-04-13 15:56:09 -0400

    changed font to droid sans mono

--- src/AirportBody/RawMetar.jsx
+++ src/AirportBody/RawMetar.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { MetarContext } from "../context/Metar";
+import "../styles/RawMetar.css"
 
 function RawMetar(){ 
     const { airportData } = useContext(MetarContext)
     
     return(
-        <div>
+        <div className="RawMetar-container">
             <h4> { airportData.raw_text } </h4>
         </div>
 )}
