d26ca325d18557042c343f33c1b8e91d1e55dc0b
Jared <jbeckersalax9@aol.com>
2023-04-12 22:11:56 -0400

    search form added

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/Search.css"
 
 function Search(){
+    const [searchParamaters, setSearchParamaters] = useState('')
+
+    function handleInputChange(event){
+        event.preventDefault()
+        setSearchParamaters(event.target.value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+    }
 
     return(
-        <div>
-
+        <div className="Search-div">
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleInputChange} className="Search-input" placeholder="Search ICAO, IATA, Name..."/>
+                <button className="Search-button">
+                    <i className="fa-solid fa-plane fa-inverse fa-2x"></i>
+                </button>
+            </form>
         </div>
 )}
 
