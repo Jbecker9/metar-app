02808d3606d71cfc3628afd916f78ffe86b48cf3
Jared <jbeckersalax9@aol.com>
2023-04-15 11:55:54 -0400

    temp buttons polished

--- src/AirportBody/AirportTemp.jsx
+++ src/AirportBody/AirportTemp.jsx
@@ -7,30 +7,23 @@ function AirportTemp(){
     const [fahrenheit, setFahrenheit] = useState(false)
     
     function renderFahrenheit(){
-        const fahrenheitElement = document.getElementById("fahrenheit")
-        const celsiusElement = document.getElementById("celsius")
         setFahrenheit(true)
-        fahrenheitElement.style.color = "#e7e7e7"
-        celsiusElement.style.color = "var(--background-color)"
+        document.getElementById("fahrenheit").style.backgroundColor = "#e7e7e7"
+        document.getElementById("celsius").style.backgroundColor = "var(--border-color)"
     }
 
     function renderCelsius(){
-        const fahrenheitElement = document.getElementById("fahrenheit")
-        const celsiusElement = document.getElementById("celsius")
         setFahrenheit(false)
-        celsiusElement.style.color = "#e7e7e7"
-        fahrenheitElement.style.color = "var(--background-color)"
+        document.getElementById("fahrenheit").style.backgroundColor = "var(--border-color)"
+        document.getElementById("celsius").style.backgroundColor = "#e7e7e7"
     }
 
     return(
     <div className="AirportTemp-container">
         <h3> { fahrenheit ? airportData.temperature.fahrenheit : airportData.temperature.celsius } </h3>
         <div className="AirportTemp-scales">
-            <h3 id="celsius" className="AirportTemp-text" onClick={renderCelsius}>C</h3>
-            <h3 id="fahrenheit" className="AirportTemp-text" onClick={renderFahrenheit}>F</h3>
-            {/* <h3 id="celsius" className="AirportTemp-text tempCelsius" onClick={renderCelsius}>C</h3>
-            <h3 className="AirportTemp-spacing">/</h3>
-            <h3 id="fahrenheit" className="AirportTemp-text" onClick={renderFahrenheit}>F</h3> */}
+            <button id="celsius" className="AirportTemp-text" onClick={()=>renderCelsius()}>C</button>
+            <button id="fahrenheit" className="AirportTemp-text" onClick={()=>renderFahrenheit()}>F</button>
         </div>
     </div>
 )}
