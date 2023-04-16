5b9ab1c266647f6303647d1f38139342c01b327f
Jared <jbeckersalax9@aol.com>
2023-04-16 00:38:04 -0400

    ready for deploy

--- src/Disclaimer.jsx
+++ src/Disclaimer.jsx
@@ -8,10 +8,10 @@ function Disclaimer(){
             <h3 className="Disclaimer-text">!! This Application is a personal project and is not intended for professional or real-time use !!</h3>
             <h4 className="Disclaimer-ownerText"> Creator: Jared Becker </h4>
             <a href="https://www.linkedin.com/in/jaredbecker-se/" >
-            <i className="fa-brands fa-linkedin fa-2x" style={{color: "#e7e7e7"}} id="linkedIn"></i>
+                <i className="fa-brands fa-linkedin fa-2x" style={{color: "#e7e7e7"}} id="linkedIn"></i>
             </a>
             <a href="https://github.com/Jbecker9">
-            <i className="fa-brands fa-square-github fa-2x" style={{color: "#e7e7e7"}} id="gitHub"></i>
+                <i className="fa-brands fa-square-github fa-2x" style={{color: "#e7e7e7"}} id="gitHub"></i>
             </a>
         </div>
 )};
