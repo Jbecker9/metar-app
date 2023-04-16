b444673369dd777cdf5b781b0bffcf4fd659f2e6
Jared <jbeckersalax9@aol.com>
2023-04-15 23:58:20 -0400

    wind direction function refactored and calls on render

--- src/AirportBody/AirportWind.jsx
+++ src/AirportBody/AirportWind.jsx
@@ -1,23 +1,21 @@
 import React, { useContext, useState } from "react";
 import { MetarContext } from "../context/Metar";
 import "../styles/AirportWind.css"
-import Compass from "cardinal-direction"
 
 function AirportWind(){
     const { airportData } = useContext(MetarContext)
-    degreeToCardinalDirection()
 
-    function degreeToCardinalDirection(){
-        const cardinalDegreeDifference = (21) + (3 / 17)
-        const denominator = 17
-        const degreeMathSort = ( airportData.wind.degrees / cardinalDegreeDifference )
-        const remainderDegree = degreeMathSort % denominator
-        console.log(remainderDegree)
+    function degreeToDirection(){
+        const cardinalDirectionsObject = [ "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" ]
+        if (airportData){
+            return cardinalDirectionsObject[Math.round( ( airportData.wind.degrees / (360 / cardinalDirectionsObject.length) )  % cardinalDirectionsObject.length )]
+        } else {
+        }
     }
 
     return(
         <div className="AirportWind-card">
-            <h3 className="windText"> { airportData.wind.speed_kts } knots - { airportData.wind.degrees } Degrees { airportData.wind.gust_kts ?  `- Gust: ${ airportData.wind.gust_kts } knots` : null } </h3>
+            <h3 className="windText"> { airportData.wind.speed_kts } knots - { airportData.wind.degrees } Degrees {degreeToDirection()} { airportData.wind.gust_kts ?  `- Gust: ${ airportData.wind.gust_kts } knots` : null } </h3>
         </div>
 )};
 
